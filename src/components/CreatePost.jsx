e3f9c3d6de4029a956d0f758e3df665a48e5f45e
Mayank Singh <msingh@cogitate.us>
2023-05-26 22:42:23 +0530

    ref#103 - Dynamic styling in styled-components

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -4,24 +4,42 @@ import { addDoc, collection } from "firebase/firestore";
 import { useFormInput } from "../hooks";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import styled, { css } from "styled-components";
 
+const StyledButton = styled.button`
+  height: 33px;
+  background: ${(props) => props.primary ? '#4caf50' : '#2196f3'};
+  border: 0;
+  color: #fff;
+  padding: 8px;
+  font-size: 15px;
+  border-radius: 3px;
+  cursor: pointer;
+
+  ${(props)=> props.primary && css`
+    &:hover {
+      border: 1px solid ${props.bgColor};
+      borderRadius: 10px;
+    }
+  `};
+`;
 const CreatePost = () => {
-  const title = useFormInput('');
-  const keyword = useFormInput('');
-  const content = useFormInput('');
+  const title = useFormInput("");
+  const keyword = useFormInput("");
+  const content = useFormInput("");
   const navigate = useNavigate();
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const docRef = await addDoc(collection(firestore, 'posts'), {
+    const docRef = await addDoc(collection(firestore, "posts"), {
       title: title.value,
       keyword: keyword.value,
       content: content.value,
-      createdAt: new Date()
-    })
+      createdAt: new Date(),
+    });
     // console.log('Document written with ID: ', docRef.id);
-    toast.success(`Document written with ID: ${docRef.id} Redirecting to Home...`)
-    navigate('/');
+    toast.success(`Document written with ID: ${docRef.id} Redirecting to Home...`);
+    navigate("/");
   };
 
   return (
@@ -41,7 +59,7 @@ const CreatePost = () => {
           <textarea {...content}></textarea>
         </div>
 
-        <button className='create-post-btn'>Create Post</button>
+        <StyledButton primary bgColor="red">Create Post</StyledButton>
       </form>
     </div>
   );
